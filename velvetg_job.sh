7e8be7f6aa34737493a73d659c19d6171dfc245f
Likit Preeyanon <preeyano@msu.edu>
2013-12-23 01:10:02 -0500

    increase memory allocation

--- velvetg_job.sh
+++ velvetg_job.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -login
-#PBS -l nodes=1:ppn=1,mem=72gb,walltime=24:00:00
+#PBS -l nodes=1:ppn=1,mem=256gb,walltime=24:00:00
 #PBS -M preeyano@msu.edu
 #PBS -m abe
 #PBS -A ged-intel11
 #PBS -N Velvetg_global_${PBS_JOBID}
-#PBS -t 21,23,25,27,29,31
+#PBS -t 21
 
 cd ${PBS_O_WORKDIR}
 velvetg global_${PBS_ARRAYID} -read_trkg yes -ins_length 175
