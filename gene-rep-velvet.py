567406ea82f84c070abd79d6411610d93bc944fb
Likit Preeyanon <preeyano@msu.edu>
2014-01-31 09:48:50 -0500

    add some new files

---
+++ gene-rep-velvet.py
@@ -0,0 +1,34 @@
+import sys
+from Bio import SeqIO
+from collections import defaultdict
+
+'''Finds the longest isoform of a gene in a FASTA file and writes
+it to stdout.
+
+'''
+
+def get_longest_gene(infile):
+    '''Returns a dictionary of sequence record objects of
+
+    the longest sequences of each gene.
+    '''
+
+    db = {}
+    for n, rec in enumerate(SeqIO.parse(infile, 'fasta'), start=1):
+        rec.id = '_'.join(rec.id.split('_')[:2])
+        try:
+            seq = db[rec.id]
+        except KeyError:
+            db[rec.id] = rec
+        else:
+            if len(seq) < len(rec):
+                db[rec.id] = rec
+        if n % 1000 == 0:
+            print >> sys.stderr, '...', n
+    return db
+
+
+if __name__=='__main__':
+    for infile in sys.argv[1:]:
+        for rec in get_longest_gene(infile).itervalues():
+            SeqIO.write(rec, sys.stdout, 'fasta')
